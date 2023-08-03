4877240207fd39879ae7129f46a91c7d884af29c
Shubham-ly <shubham.m@ly.design>
2023-08-03 16:19:45 +0530

    Changed lead creation routes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,26 +3,29 @@ import { Route, Routes } from 'react-router-dom';
 import AuthContextLayout from '../context/AuthContext';
 import Dashboard from './dashboard';
 
-const AddressDetails = lazy(() => import('./address-details'));
-const ApplicantDetails = lazy(() => import('./applicant-details'));
-const BankingDetails = lazy(() => import('./banking-details'));
-const LntCharges = lazy(() => import('./lnt-charges'));
-const PersonalDetails = lazy(() => import('./personal-details'));
-const ReferenceDetails = lazy(() => import('./reference-details'));
-const WorkIncomeDetails = lazy(() => import('./work-income-details'));
+const AddressDetails = lazy(() => import('./dashboard/lead-creation/address-details'));
+const ApplicantDetails = lazy(() => import('./dashboard/lead-creation/applicant-details'));
+const BankingDetails = lazy(() => import('./dashboard/lead-creation/banking-details'));
+const LntCharges = lazy(() => import('./dashboard/lead-creation/lnt-charges'));
+const PersonalDetails = lazy(() => import('./dashboard/lead-creation/personal-details'));
+const ReferenceDetails = lazy(() => import('./dashboard/lead-creation/reference-details'));
+const WorkIncomeDetails = lazy(() => import('./dashboard/lead-creation/work-income-details'));
 
 const DashboardRoutes = () => {
   return (
     <Routes>
       <Route element={<AuthContextLayout />}>
         <Route path='/' element={<Dashboard />} />
-        <Route path='/address-details' element={<AddressDetails />} />
-        <Route path='/applicant-details' element={<ApplicantDetails />} />
-        <Route path='/banking-details' element={<BankingDetails />} />
-        <Route path='/lnt-charges' element={<LntCharges />} />
-        <Route path='/personal-details' element={<PersonalDetails />} />
-        <Route path='/reference-details' element={<ReferenceDetails />} />
-        <Route path='/work-income-details' element={<WorkIncomeDetails />} />
+        <Route path='/lead'>
+          <Route index element={<ApplicantDetails />} />
+          <Route path='applicant-details' element={<ApplicantDetails />} />
+          <Route path='address-details' element={<AddressDetails />} />
+          <Route path='banking-details' element={<BankingDetails />} />
+          <Route path='lnt-charges' element={<LntCharges />} />
+          <Route path='personal-details' element={<PersonalDetails />} />
+          <Route path='reference-details' element={<ReferenceDetails />} />
+          <Route path='work-income-details' element={<WorkIncomeDetails />} />
+        </Route>
       </Route>
     </Routes>
   );
