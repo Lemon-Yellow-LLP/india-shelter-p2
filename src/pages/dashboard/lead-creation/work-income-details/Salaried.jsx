1d5a4e946ebe3aec97d1e2b1acd4ca54fe9d1081
PrathameshShingateLy <prathamesh@ly.design>
2023-09-07 12:45:58 +0530

    Changes to work and income form and fields

---
+++ src/pages/dashboard/lead-creation/work-income-details/Salaried.jsx
@@ -0,0 +1,227 @@
+import { useCallback, useContext, useState } from 'react';
+import { AuthContext } from '../../../../context/AuthContext';
+import TextInput from '../../../../components/TextInput';
+import DropDown from '../../../../components/DropDown';
+import SearchableTextInput from '../../../../components/TextInput/SearchableTextInput';
+import CardRadioWithoutIcon from '../../../../components/CardRadio/CardRadioWithoutIcon';
+import { noOfDependentsOptions, totalFamilyMembersOptions } from '../utils';
+// import { top100Films } from '../../../../assets/SearchableInputTestJsonData.json';
+
+export default function Salarid() {
+  const { values, setValues, errors } = useContext(AuthContext);
+
+  const handleTextInputChange = useCallback((e) => {
+    let newData = values;
+    newData[e.target.name] = e.target.value;
+    setValues(newData);
+  }, []);
+
+  const handleRadioChange = useCallback((e) => {
+    let newData = values;
+    newData[e.name] = e.value;
+    setValues(newData);
+  }, []);
+
+  const salaridDropdownOptions = [
+    {
+      title: 'No of Current Loans',
+      options: [
+        {
+          label: '1',
+          value: 1,
+        },
+        {
+          label: '2',
+          value: 2,
+        },
+        {
+          label: '3',
+          value: 3,
+        },
+      ],
+    },
+  ];
+
+  const handleDropdownChange = () => {};
+
+  return (
+    <>
+      {/* <SearchableTextInput
+        label='Company name'
+        placeholder='Eg: Idea'
+        required
+        name='company_name'
+        value={values.company_name}
+        onChange={handleTextInputChange}
+        type='search'
+        options={top100Films}
+      /> */}
+
+      <TextInput
+        label='Total income'
+        placeholder='Eg: 10,000'
+        required
+        name='total_income'
+        value={values.total_income}
+        onChange={handleTextInputChange}
+      />
+
+      <TextInput
+        label='UTRN'
+        placeholder='Eg: 1234567'
+        name='utrn'
+        value={values.utrn}
+        onChange={handleTextInputChange}
+      />
+
+      <DropDown
+        label='No. of current loan(s)'
+        required
+        options={salaridDropdownOptions[0].options}
+        placeholder='Choose no. of current loan(s)'
+        onChange={handleDropdownChange}
+        defaultSelected={values.no_of_current_loans}
+      />
+
+      <TextInput
+        label='Ongoing EMI(s)'
+        placeholder='Eg: 10,000'
+        required
+        name='ongoing_emi'
+        value={values.ongoing_emi}
+        onChange={handleTextInputChange}
+        hint='Total ongoing EMI(s) based on the ongoing loan(s)'
+      />
+
+      <DropDown
+        label='Working since'
+        required
+        options={salaridDropdownOptions[0].options}
+        placeholder='Choose working since'
+        onChange={handleDropdownChange}
+        defaultSelected={values.working_since}
+      />
+
+      <DropDown
+        label='Mode of salary'
+        required
+        options={salaridDropdownOptions[0].options}
+        placeholder='Choose mode of salary'
+        onChange={handleDropdownChange}
+        defaultSelected={values.mode_of_salary}
+      />
+
+      <TextInput
+        label='Flat no/Building name'
+        placeholder='Eg: C-101'
+        required
+        name='flat_no'
+        value={values.flat_no}
+        onChange={handleTextInputChange}
+      />
+
+      <TextInput
+        label='Street/Area/Locality'
+        placeholder='Eg: Senapati road'
+        required
+        name='street_area_locality'
+        value={values.street_area_locality}
+        onChange={handleTextInputChange}
+      />
+
+      <TextInput
+        label='Town'
+        placeholder='Eg: Igatpuri'
+        name='town'
+        value={values.town}
+        onChange={handleTextInputChange}
+      />
+
+      <TextInput
+        label='Landmark'
+        placeholder='Eg: Near apollo hospital'
+        required
+        name='landmark'
+        value={values.landmark}
+        onChange={handleTextInputChange}
+      />
+
+      <TextInput
+        label='Pincode'
+        placeholder='Eg: 123456'
+        required
+        name='pincode'
+        value={values.pincode}
+        onChange={handleTextInputChange}
+        hint='City and State fields will get filled based on Pincode'
+      />
+
+      <TextInput
+        label='City'
+        placeholder='Eg: Nashik'
+        name='city'
+        value={values.city}
+        onChange={handleTextInputChange}
+      />
+
+      <TextInput
+        label='State'
+        placeholder='Eg: Maharashtra'
+        name='state'
+        value={values.state}
+        onChange={handleTextInputChange}
+      />
+
+      <div className='flex flex-col gap-2'>
+        <label htmlFor='loan-purpose' className='flex gap-0.5 font-medium text-black'>
+          Total family members <span className='text-primary-red text-xs'>*</span>
+        </label>
+        <div className={`flex gap-4 w-full`}>
+          {totalFamilyMembersOptions.map((option) => {
+            return (
+              <CardRadioWithoutIcon
+                key={option.value}
+                label={option.label}
+                name='total_family_members'
+                value={option.value}
+                current={values.total_family_members}
+                onChange={handleRadioChange}
+                containerClasses='flex-1'
+              ></CardRadioWithoutIcon>
+            );
+          })}
+        </div>
+      </div>
+
+      <TextInput
+        label='Total household income'
+        placeholder='₹ 1,00,000'
+        required
+        name='total_household_income'
+        value={values.total_household_income}
+        onChange={handleTextInputChange}
+      />
+
+      <div className='flex flex-col gap-2'>
+        <label htmlFor='loan-purpose' className='flex gap-0.5 font-medium text-black'>
+          No. of Dependents <span className='text-primary-red text-xs'>*</span>
+        </label>
+        <div className={`flex gap-4 w-full`}>
+          {noOfDependentsOptions.map((option) => {
+            return (
+              <CardRadioWithoutIcon
+                key={option.value}
+                label={option.label}
+                name='no_of_dependents'
+                value={option.value}
+                current={values.no_of_dependents}
+                onChange={handleRadioChange}
+                containerClasses='flex-1'
+              ></CardRadioWithoutIcon>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
