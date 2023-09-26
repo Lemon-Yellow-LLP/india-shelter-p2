f246de20c685cff48a05114f6f0b7c763c6b8635
PrathameshShingateLy <prathamesh@ly.design>
2023-09-26 18:45:39 +0530

    upload documents intial commit

--- src/pages/dashboard/lead-creation/upload-documents/index.jsx
+++ src/pages/dashboard/lead-creation/upload-documents/index.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import ImageUpload from '../../../../components/ImageUpload';
+
 const UploadDocuments = () => {
+  const [files, setFile] = useState([]);
+  const labelClassName = 'mb-2 text-lg font-medium text-gray-900';
+
   return (
     <div>
-      <h1>UploadDocuments</h1>
+      <div>
+        {/* <label htmlFor='image' className={labelClassName}>
+          Gig Images
+        </label> */}
+        <div>
+          <ImageUpload files={files} setFile={setFile} />
+        </div>
+      </div>
     </div>
   );
 };
