e52fdea4cbad5490101b15f6d33bbaea3cfb9462
PrathameshShingateLy <prathamesh@ly.design>
2023-09-27 18:33:40 +0530

    changed popup component

--- src/components/DesktopPopUp/index.jsx
+++ src/components/DesktopPopUp/index.jsx
@@ -1,8 +1,9 @@
 import { createPortal } from 'react-dom';
 import { IconClose } from '../../assets/icons';
 import PropTypes from 'prop-types';
+import Button from '../Button';
 
-const DesktopPopUp = ({ showpopup, setShowPopUp, children, title }) => {
+const DesktopPopUp = ({ showpopup, setShowPopUp, img, callback }) => {
   if (showpopup)
     return createPortal(
       <div
@@ -14,27 +15,26 @@ const DesktopPopUp = ({ showpopup, setShowPopUp, children, title }) => {
         }}
         className='fixed inset-0 w-full bg-black bg-opacity-50'
       >
-        <div
-          style={{
-            width: 646,
-          }}
-          className='hidden md:flex absolute top-2/4 -translate-y-2/4 left-2/4 -translate-x-2/4 rounded-lg shadow-lg flex-col w-full bg-white outline-none focus:outline-none'
-        >
-          <div className='flex items-start justify-between py-6 pl-6 pr-4 border-b border-solid border-slate-200 rounded-t'>
-            <h3 className='text-xl text-primary-black font-semibold'>{title}</h3>
-            <button className='p-1 ml-auto' onClick={() => setShowPopUp(false)}>
+        <div className='w-[328px] flex absolute top-2/4 -translate-y-2/4 left-2/4 -translate-x-2/4 rounded-lg shadow-lg flex-col outline-none focus:outline-none'>
+          <div className='flex items-start justify-between bg-transparent'>
+            <button className='ml-auto' onClick={() => setShowPopUp(false)}>
               <IconClose />
             </button>
           </div>
-          <div className='py-4 pl-6 pr-8'>
-            <div style={{ height: 438 }} className='relative pr-4 flex-auto overflow-y-auto'>
-              <p
-                className='text-base text-dark-grey font-normal leading-relaxed'
-                dangerouslySetInnerHTML={{
-                  __html: children,
-                }}
-              />
-            </div>
+          <div className='h-[437px] bg-white mt-3 rounded-t-lg'>
+            <img
+              src={URL.createObjectURL(img)}
+              alt={img}
+              className='h-full w-full object-cover object-center rounded-t-lg'
+            />
+          </div>
+          <div className='p-4 flex gap-4 bg-white rounded-b-lg'>
+            <Button inputClasses='w-full' onClick={() => callback(img)}>
+              Delete
+            </Button>
+            <Button inputClasses='w-full' primary>
+              Retake
+            </Button>
           </div>
         </div>
       </div>,
@@ -48,6 +48,5 @@ export default DesktopPopUp;
 DesktopPopUp.propTypes = {
   showpopup: PropTypes.bool,
   setShowPopUp: PropTypes.func,
-  children: PropTypes.elementType,
-  title: PropTypes.string,
+  img: PropTypes.object,
 };
