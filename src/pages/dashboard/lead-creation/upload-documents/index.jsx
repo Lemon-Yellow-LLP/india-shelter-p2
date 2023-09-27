a2fbf79eb8f135a70ae42c936b66a085938a3516
PrathameshShingateLy <prathamesh@ly.design>
2023-09-27 18:35:16 +0530

    upload documents page ui

--- src/pages/dashboard/lead-creation/upload-documents/index.jsx
+++ src/pages/dashboard/lead-creation/upload-documents/index.jsx
@@ -1,19 +1,65 @@
 import { useState } from 'react';
 import ImageUpload from '../../../../components/ImageUpload';
+import PdfAndImageUpload from '../../../../components/PdfAndImageUpload';
+import PhotoUpload from '../../../../components/PhotoUpload';
 
 const UploadDocuments = () => {
-  const [files, setFile] = useState([]);
-  const labelClassName = 'mb-2 text-lg font-medium text-gray-900';
+  const [customerPhotos, setCustomerPhotos] = useState([]);
+  const [idProofPhotos, setIdProofPhotos] = useState([]);
+  const [addressProofPhotos, setAddressProofPhotos] = useState([]);
+  const [propertyPapers, setPropertyPapers] = useState([]);
+  const [salarySlipPhotos, setSalarySlipPhotos] = useState([]);
+  const [form60photos, setForm60photos] = useState([]);
+  const [propertyPhotos, setPropertyPhotos] = useState([]);
+  const [selfie, setSelfie] = useState([]);
+  const [docs, setDocs] = useState([]);
+  // const labelClassName = 'mb-2 text-lg font-medium text-gray-900';
 
   return (
     <div>
-      <div>
-        {/* <label htmlFor='image' className={labelClassName}>
-          Gig Images
-        </label> */}
-        <div>
-          <ImageUpload files={files} setFile={setFile} />
-        </div>
+      <div className='p-4 flex flex-col gap-9'>
+        <PhotoUpload
+          files={customerPhotos}
+          setFile={setCustomerPhotos}
+          label='Customer photo'
+          required
+        />
+
+        <ImageUpload files={idProofPhotos} setFile={setIdProofPhotos} label='ID proof' required />
+
+        <ImageUpload
+          files={addressProofPhotos}
+          setFile={setAddressProofPhotos}
+          label='Address proof'
+          required
+        />
+
+        <PdfAndImageUpload
+          files={propertyPapers}
+          setFile={setPropertyPapers}
+          label='Property papers'
+          required
+        />
+
+        <ImageUpload
+          files={salarySlipPhotos}
+          setFile={setSalarySlipPhotos}
+          label='Salary slip'
+          required
+        />
+
+        <ImageUpload files={form60photos} setFile={setForm60photos} label='Form 60' required />
+
+        <ImageUpload
+          files={propertyPhotos}
+          setFile={setPropertyPhotos}
+          label='Property image'
+          required
+        />
+
+        <PhotoUpload files={selfie} setFile={setSelfie} label='Upload selfie' required />
+
+        <ImageUpload files={docs} setFile={setDocs} label='Other documents' required />
       </div>
     </div>
   );
