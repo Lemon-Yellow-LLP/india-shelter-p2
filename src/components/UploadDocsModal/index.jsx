3965e8b19afbb057d07a9d2c175f6c7721516645
PrathameshShingateLy <prathamesh@ly.design>
2023-10-09 19:05:42 +0530

    added slider functionality for images

--- src/components/UploadDocsModal/index.jsx
+++ src/components/UploadDocsModal/index.jsx
@@ -2,14 +2,17 @@ import { createPortal } from 'react-dom';
 import { IconClose } from '../../assets/icons';
 import PropTypes from 'prop-types';
 import Button from '../Button';
+import { useState } from 'react';
 
-const UploadDocsModal = ({ showpopup, setShowPopUp, img, callback, lat, long }) => {
+const UploadDocsModal = ({ showpopup, setShowPopUp, img, callback, lat, long, photos }) => {
+  const [activeStep, setActiveStep] = useState(0);
+
   if (showpopup)
     return createPortal(
       <div
         role='presentation'
-        onClick={() => setShowPopUp(false)}
-        onKeyDown={() => setShowPopUp(false)}
+        // onClick={() => setShowPopUp(false)}
+        // onKeyDown={() => setShowPopUp(false)}
         style={{
           zIndex: 9999999,
         }}
@@ -21,20 +24,104 @@ const UploadDocsModal = ({ showpopup, setShowPopUp, img, callback, lat, long }) 
               <IconClose />
             </button>
           </div>
+
           <div className='relative h-[437px] bg-white mt-3 rounded-t-lg'>
-            <img
-              src={img}
-              alt={img}
-              className='h-full w-full object-cover object-center rounded-t-lg'
-            />
-            <p className='absolute bottom-0 left-0 text-white p-3'>
-              Lat: {lat}, Long: {long}
-            </p>
+            {photos.map(
+              (photo, index) =>
+                index === activeStep && (
+                  <div key={index}>
+                    <img
+                      src={photo.document_url}
+                      alt={photo.document_url}
+                      className='h-full w-full object-cover object-center rounded-t-lg'
+                    />
+                    <p className='absolute bottom-0 left-0 text-white p-3'>
+                      Lat: {lat}, Long: {long}
+                    </p>
+                  </div>
+                ),
+            )}
+
+            {/* {img && index !== activeStep && (
+              <>
+                <img
+                  src={img}
+                  alt={img}
+                  className='h-full w-full object-cover object-center rounded-t-lg'
+                />
+                <p className='absolute bottom-0 left-0 text-white p-3'>
+                  Lat: {lat}, Long: {long}
+                </p>
+              </>
+            )} */}
           </div>
+
           <div className='p-4 flex gap-4 bg-white rounded-b-lg'>
             <Button inputClasses='w-full' onClick={() => callback(img)}>
               Delete
             </Button>
+          </div>
+
+          <div className='flex items-start justify-between bg-transparent mt-4'>
+            {
+              <div
+                className={`bg-white border-[1px] border-[#E2EAF4] text-primary-black h-10 w-10 rounded-full flex justify-center items-center cursor-pointer ${
+                  activeStep === 0 ? 'pointer-events-none opacity-80' : 'pointer-events-auto'
+                }`}
+                onClick={() => {
+                  setActiveStep((prev) => prev - 1);
+                  callback(activeStep - 1);
+                }}
+              >
+                <svg
+                  width='32'
+                  height='32'
+                  viewBox='0 0 32 32'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <rect width='32' height='32' rx='16' fill='#FEFEFE' />
+                  <path
+                    d='M19 10L13 16L19 22'
+                    stroke='#373435'
+                    strokeWidth='1.5'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  />
+                </svg>
+              </div>
+            }
+
+            {
+              <div
+                className={`bg-white border-[1px] border-[#E2EAF4] text-primary-black h-10 w-10 rounded-full flex justify-center items-center cursor-pointer ${
+                  activeStep === photos.length - 1
+                    ? 'pointer-events-none opacity-80'
+                    : 'pointer-events-auto'
+                }`}
+                onClick={() => {
+                  setActiveStep((prev) => prev + 1);
+                  callback(activeStep + 1);
+                }}
+              >
+                <svg
+                  width='32'
+                  height='32'
+                  viewBox='0 0 32 32'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <rect width='32' height='32' rx='16' fill='white' />
+                  <path
+                    d='M13 22L19 16L13 10'
+                    stroke='#373435'
+                    strokeWidth='1.5'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  />
+                </svg>
+              </div>
+            }
           </div>
         </div>
       </div>,
