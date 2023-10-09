0f9e3e80fe6c66c112e67826d3d734ed72c274a8
PrathameshShingateLy <prathamesh@ly.design>
2023-10-09 19:04:51 +0530

    added upload doc api calling functions

--- src/global/index.js
+++ src/global/index.js
@@ -243,6 +243,8 @@ async function checkBre101(id, options) {
   return res.data;
 }
 
+// UPLOAD DOCUMENT
+
 async function uploadDoc(data, options) {
   const res = await axios.post(`${API_URL}/doc/upload/document`, data, options);
   return res.data;
@@ -251,6 +253,21 @@ async function uploadDoc(data, options) {
 async function getApplicantById(id) {
   const res = await axios.get(`${API_URL}/applicant/${id}`);
   return res.data;
+}
+
+async function reUploadDoc(id, data, options) {
+  const res = await axios.post(`${API_URL}/doc/upload/document/re-upload/${id}`, data, options);
+  return res.data;
+}
+
+async function getUploadOtp(id) {
+  const res = await axios.get(`${API_URL}/account/send-sms/${id}`, {}, requestOptions);
+  return res;
+}
+
+async function verifyUploadOtp(id, otp) {
+  const res = await axios.post(`${API_URL}/account/verify-sms/${id}`, { otp }, requestOptions);
+  return res;
 }
 
 async function updateLeadDataOnBlur(leadId, fieldName, value) {
@@ -473,4 +490,7 @@ export {
   checkBre101,
   uploadDoc,
   getApplicantById,
+  getUploadOtp,
+  verifyUploadOtp,
+  reUploadDoc,
 };
