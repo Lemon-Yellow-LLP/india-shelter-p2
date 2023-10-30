ba0457e9c789e252b138ae14b830fcc2eaa711b4
ChetanDigholeLY <chetan.d@ly.design>
2023-10-30 12:49:25 +0530

    fixed gst issue

--- src/pages/dashboard/lead-creation/work-income-details/SelfEmployed.jsx
+++ src/pages/dashboard/lead-creation/work-income-details/SelfEmployed.jsx
@@ -167,7 +167,7 @@ export default function SelfEmployed({ requiredFieldsStatus, setRequiredFieldsSt
       <TextInput
         label='GST number'
         placeholder='Eg: 06AAAPB2117A1ZI'
-        className='uppercase'
+        // className='uppercase'
         name={`applicants[${activeIndex}].work_income_detail.gst_number`}
         value={values?.applicants?.[activeIndex]?.work_income_detail?.gst_number}
         error={errors?.applicants?.[activeIndex]?.work_income_detail?.gst_number}
@@ -202,6 +202,7 @@ export default function SelfEmployed({ requiredFieldsStatus, setRequiredFieldsSt
           }
         }}
         onChange={(e) => {
+          e.target.value = e.target.value.toUpperCase();
           const value = e.currentTarget.value;
           const pattern = /^[a-zA-Z0-9]+$/;
           if (!pattern.test(value) && value.length > 0) {
