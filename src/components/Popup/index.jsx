e9bf7e3363e105e4a16fc2d174fe29b19745ed88
akshay <akshay.shinde@ly.design>
2024-01-09 15:10:22 +0530

    popup state added

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,30 +1,75 @@
 import React from 'react';
+import loading from '../../assets/icons/loading.svg';
 
-export default function Popup({ onClick, handleClose, open, setOpen, title, description }) {
+export default function Popup({
+  onClick,
+  handleClose,
+  open,
+  setOpen,
+  title,
+  description,
+  bottom,
+  state,
+}) {
+  if (state === 'success') {
+    setTimeout(() => {
+      setOpen(false);
+    }, 1000);
+  }
+  let popupStateClasses =
+    state === 'error'
+      ? 'bg-lighter-red border border-primary-red'
+      : state === 'success'
+      ? 'bg-light-green border border-secondary-green'
+      : 'bg-[#000000F2]';
+  let popupTextColor =
+    state === 'error' || state === 'success' ? 'text-primary-black' : 'text-neutral-white';
+  let crossSvgColor = state === 'error' || state === 'success' ? '#373435' : '#FEFEFE';
   return open ? (
-    <div className='absolute bottom-[145px] w-full flex items-center p-[20px] z-[900]'>
-      <div className=' flex-1 w-[100%] p-[12px] flex flex-col gap-3 rounded bg-[#000000F2] shadow-[0px_8px_32px_0px_rgba(0_0_0_0.20)]'>
-        <div className='flex gap-2 items-start'>
+    <div
+      style={{ bottom: `${bottom || 145}px` }}
+      className='absolute w-full flex items-center p-[20px] z-[900]'
+    >
+      <div
+        className={`flex-1 w-[100%] p-[12px] flex flex-col gap-3 rounded ${popupStateClasses} shadow-[0px_8px_32px_0px_rgba(0_0_0_0.20)]`}
+      >
+        <div className='flex gap-2 items-center'>
+          {state === 'loading' && (
+            <div className='ml-auto'>
+              <img src={loading} alt='loading' className='animate-spin duration-300 ease-out' />
+            </div>
+          )}
           <div className='flex-1 flex flex-col gap-[2px]'>
-            <h4 className='text-[14px] not-italic font-normal text-neutral-white'>{title}</h4>
-            <p className='not-italic font-normal text-[11px] leading-4 text-light-grey'>
-              {description}
-            </p>
+            {title && (
+              <h4 className={`text-[14px] not-italic font-normal ${popupTextColor}`}>{title}</h4>
+            )}
+            {description && (
+              <p className='not-italic font-normal text-[11px] leading-4 text-light-grey'>
+                {description}
+              </p>
+            )}
           </div>
-          <button onClick={handleClose} className='p-[6px]'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='12'
-              height='12'
-              fill='none'
-              viewBox='0 0 12 12'
-            >
-              <g stroke='#FEFEFE' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.5'>
-                <path d='M11 1L1 11'></path>
-                <path d='M1 1l10 10'></path>
-              </g>
-            </svg>
-          </button>
+          {state !== 'success' && (
+            <button onClick={handleClose} className='p-[6px] self-start'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='12'
+                height='12'
+                fill='none'
+                viewBox='0 0 12 12'
+              >
+                <g
+                  stroke={`${crossSvgColor}`}
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='1.5'
+                >
+                  <path d='M11 1L1 11'></path>
+                  <path d='M1 1l10 10'></path>
+                </g>
+              </svg>
+            </button>
+          )}
         </div>
         {/* 
         <button onClick={onClick} className='ml-auto'>
