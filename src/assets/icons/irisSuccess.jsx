5874c8a2a9679836f9ae700741f913b623a3bde4
akshay <akshay.shinde@ly.design>
2024-01-10 12:04:39 +0530

    svgs added

---
+++ src/assets/icons/irisSuccess.jsx
@@ -0,0 +1,28 @@
+export default function IrisSuccess() {
+  return (
+    <svg width='48' height='48' viewBox='0 0 48 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M24 29C25.3261 29 26.5979 28.4732 27.5355 27.5355C28.4732 26.5979 29 25.3261 29 24C29 22.6739 28.4732 21.4021 27.5355 20.4645C26.5979 19.5268 25.3261 19 24 19C22.6739 19 21.4021 19.5268 20.4645 20.4645C19.5268 21.4021 19 22.6739 19 24C19 25.3261 19.5268 26.5979 20.4645 27.5355C21.4021 28.4732 22.6739 29 24 29Z'
+        stroke='#147257'
+        strokeWidth='3'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M46.5 24C41.7775 31.4775 33.295 39 24 39C14.705 39 6.2225 31.4775 1.5 24C7.2475 16.895 13.98 9 24 9C34.02 9 40.7525 16.895 46.5 24Z'
+        stroke='#147257'
+        strokeWidth='3'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <rect x='30' y='30' width='16' height='16' rx='8' fill='#147257' />
+      <path
+        d='M42.6673 34.5L36.2507 40.9167L33.334 38'
+        stroke='#FEFEFE'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}
