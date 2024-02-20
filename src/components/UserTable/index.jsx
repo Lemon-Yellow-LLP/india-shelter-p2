97d25a970898edfdd717c5a9f389ffa4e8ed9232
PrathameshShingateLy <prathamesh@ly.design>
2024-02-20 17:04:58 +0530

    added empty states

--- src/components/UserTable/index.jsx
+++ src/components/UserTable/index.jsx
@@ -1,3 +1,4 @@
+import NoUsersOnSearchIcon from '../../assets/icons/NoUsersOnSearch';
 import UserRow from '../UserRow';
 
 const TableHeaderList = [
@@ -14,25 +15,32 @@ const TableHeaderList = [
 const UserTable = ({ userslist }) => {
   return (
     <div className='custom-table h-[570px]'>
-      <table className='w-full'>
-        <thead>
-          <tr>
-            {TableHeaderList.map((heading, i) => (
-              <th
-                className='text-dark-grey font-medium text-xs leading-5 p-4 bg-white text-left'
-                key={i}
-              >
-                {heading}
-              </th>
+      {!userslist.length ? (
+        <div className='flex justify-center items-center h-full'>
+          <NoUsersOnSearchIcon />
+        </div>
+      ) : (
+        <table className=' w-full'>
+          <thead>
+            <tr>
+              {TableHeaderList.map((heading, i) => (
+                <th
+                  className='text-dark-grey font-medium text-xs leading-5 p-4 bg-white text-left'
+                  key={i}
+                >
+                  {heading}
+                </th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody>
+            {userslist.map((user, i) => (
+              <UserRow user={user} key={user.id} i={i} />
             ))}
-          </tr>
-        </thead>
-        <tbody>
-          {userslist.map((user, i) => (
-            <UserRow user={user} key={user.id} i={i} />
-          ))}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
